69d4e4a9af90c57238031a89add3f4486559c18a
Brandon Vargas <bvargas@audisoft.com>
2022-11-30 23:24:27 -0400

    BV: change dependencies version @angular/cli

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'rifas-app';
+  /**
+    @DefaultConstruct
+  */
+  constructor(private primengConfig: PrimeNGConfig){
+  }
+  /**
+    @implements
+  */
+  ngOnInit() {
+    this.primengConfig.ripple = true;
+  }
+
 }
