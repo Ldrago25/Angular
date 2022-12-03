921f60e5d8b341a71174d079d39b44ee25577b7e
CarlosAl96 <carlos96cacv@gmail.com>
2022-12-02 20:26:50 -0400

    CC: add primeflex css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AutoCompleteModule } from "primeng/autocomplete";
+import { AutoCompleteModule } from 'primeng/autocomplete';
 import { RafflesComponent } from 'src/raffles-module/raffles-module.component';
 import { MenuComponent } from 'src/menuComponente-module/menu-component-module.component';
-import { InputTextModule } from "primeng/inputtext";
+import { InputTextModule } from 'primeng/inputtext';
+import { CardModule } from 'primeng/card';
+import { InputTextareaModule } from 'primeng/inputtextarea';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    RafflesComponent,
-    MenuComponent
-  ],
+  declarations: [AppComponent, RafflesComponent, MenuComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AutoCompleteModule,
-    InputTextModule
+    InputTextModule,
+    CardModule,
+    InputTextareaModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
