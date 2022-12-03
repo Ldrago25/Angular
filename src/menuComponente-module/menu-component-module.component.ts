1834e6ae5ee1e59e85c35a547749e3bef779e8e8
jeffersonenr02 <jnieto0222@gmail.com>
2022-12-02 20:25:44 -0400

    JN: "edit"

--- src/menuComponente-module/menu-component-module.component.ts
+++ src/menuComponente-module/menu-component-module.component.ts
@@ -1,17 +1,26 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Data, Router } from '@angular/router';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import {MenuItem} from 'primeng/api';
+
 
 @Component({
   selector: 'app-menu',
-  templateUrl: './menu-component-module.component.html'
+  templateUrl: './menu-component-module.component.html',
+  styleUrls: ['./menu-component-module.component.css']
 })
 export class MenuComponent implements OnInit {
+
+  items: MenuItem[] = [];
 
   constructor() { }
 
 
   ngOnInit(): void {
-
+    this.items = [
+      {label: 'New', icon: 'pi pi-fw pi-plus'},
+      {label: 'Open', icon: 'pi pi-fw pi-download'},
+      {label: 'Undo', icon: 'pi pi-fw pi-refresh'}
+  ];
   }
 }
