bdbc64539a28c10a97c3d36b353278dcba6838ab
CarlosAl96 <carlos96cacv@gmail.com>
2022-12-05 17:59:01 -0400

    CC: add details raffles view

--- src/raffles-module/raffles-module.component.ts
+++ src/raffles-module/raffles-module.component.ts
@@ -13,15 +13,21 @@ interface Raffle {
   selector: 'app-raffles',
   templateUrl: './raffles-module.component.html',
   styleUrls: ['./raffles-module.component.css'],
+  styles: [
+    `
+      :host ::ng-deep table {
+        width: 100%;
+      }
+    `,
+  ],
 })
 export class RafflesComponent implements OnInit {
   raffles: Raffle[] = [];
 
   constructor() {}
 
-  change(event: any){
+  change(event: any) {
     console.log(event);
-    
   }
 
   ngOnInit(): void {
@@ -30,31 +36,31 @@ export class RafflesComponent implements OnInit {
         name: 'Rifa de moto',
         price: 200,
         tickets: 1000,
-        date: new Date().toLocaleDateString("es"),
+        date: new Date().toLocaleDateString('es'),
       },
       {
         name: 'Rifa de carro',
         price: 300,
         tickets: 2000,
-        date: new Date().toLocaleDateString("es"),
+        date: new Date().toLocaleDateString('es'),
       },
       {
         name: 'Rifa de telefono',
         price: 500,
         tickets: 7000,
-        date: new Date().toLocaleDateString("es"),
+        date: new Date().toLocaleDateString('es'),
       },
       {
         name: 'Rifa de casa',
         price: 800,
         tickets: 10000,
-        date: new Date().toLocaleDateString("es"),
+        date: new Date().toLocaleDateString('es'),
       },
       {
         name: 'Rifa de apartamento',
         price: 1000,
         tickets: 10000,
-        date: new Date().toLocaleDateString("es"),
+        date: new Date().toLocaleDateString('es'),
       },
     ];
   }
