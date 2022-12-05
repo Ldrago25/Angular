a0027230b8d2b8295e78369e80af0b0792f069b0
CarlosAl96 <carlos96cacv@gmail.com>
2022-12-05 19:16:22 -0400

    CC: add raffles user component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AutoCompleteModule } from 'primeng/autocomplete';
 import { RafflesComponent } from 'src/raffles-module/raffles-module.component';
+import { RafflesUserComponent } from 'src/raffles-user-module/raffles-user-module.component';
 import { ParticipantComponent } from 'src/participants-module/participants-module.component';
 import { MenuComponent } from 'src/menuComponente-module/menu-component-module.component';
 import { InputTextModule } from 'primeng/inputtext';
@@ -16,7 +17,13 @@ import { CalendarModule } from 'primeng/calendar';
 import { TableModule } from 'primeng/table';
 
 @NgModule({
-  declarations: [AppComponent, RafflesComponent, ParticipantComponent, MenuComponent],
+  declarations: [
+    AppComponent,
+    RafflesComponent,
+    ParticipantComponent,
+    RafflesUserComponent,
+    MenuComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
